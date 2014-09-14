314d32c89e96cdbedb416cab7e00ee8e5abb285b
davlin93 <dlin@clypd.com>
2014-09-14 18:37:08 -0400

    use new tsunami app creds

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, 356821657808253, '5b7eb29f703e13d1eaf6a3e10172fbf1'
+  provider :facebook, 820271541324765, 'a7ea31e7f43e028ff634afa2ad7d6b7c'
 end
