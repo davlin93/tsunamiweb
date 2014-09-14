7e003faebc74dcc44aa790b633948ee2a829748c
davlin <david@davidalin.com>
2014-09-13 23:45:31 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tsunami::Application.config.secret_token = 'a7ffec2be4cdbc66bef187ec0fb4c15cb09b78085032e8c8de5dedafbebaca44ab2ee60462630a0af214e5d04f0253e6af5a479c5f1a5f698946623234abd5e6'
