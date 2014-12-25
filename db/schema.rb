28e5b9014b4d3df9cd6358d847f91ae0d56f53fc
davlin93 <dlin@clypd.com>
2014-12-24 23:13:15 -0500

    log view records

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141127161249) do
+ActiveRecord::Schema.define(:version => 20141224185014) do
 
   create_table "contents", :force => true do |t|
     t.string  "title"
@@ -40,6 +40,13 @@ ActiveRecord::Schema.define(:version => 20141127161249) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.string   "guid"
+  end
+
+  create_table "view_records", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "wave_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "waves", :force => true do |t|
